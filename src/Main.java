ecb49c65dfea55945d535df5f63ed5f514b7b6c3
PedroBarge <pedro525tavares@gmail.com>
2024-01-08 13:59:35 +0000

    i think its done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Fazer uma função recursiva que calcule a sequência de Fibonacci");
+
+        System.out.print("Write a number: ");
+        Scanner scanner = new Scanner(System.in);
+        int numChoice = scanner.nextInt();
+        System.out.println(fibonacci(numChoice));
+    }
+
+    private static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
     }
 }
